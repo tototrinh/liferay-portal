37f85ef860ccf2dd25b7aeeb3ed828e48cd1b8c3
tototrinh <to.trinh@codeenginestudio.com>
2023-07-10 10:57:42 +0700

    LPS-154515 Add PermissionPropagation checkbox to the permission modal

--- modules/apps/portlet-configuration/portlet-configuration-web/src/main/java/com/liferay/portlet/configuration/web/internal/display/context/PortletConfigurationPermissionsDisplayContext.java
+++ modules/apps/portlet-configuration/portlet-configuration-web/src/main/java/com/liferay/portlet/configuration/web/internal/display/context/PortletConfigurationPermissionsDisplayContext.java
@@ -27,6 +27,8 @@ import com.liferay.portal.kernel.portlet.LiferayWindowState;
 import com.liferay.portal.kernel.portlet.PortletProvider;
 import com.liferay.portal.kernel.portlet.PortletProviderUtil;
 import com.liferay.portal.kernel.portlet.PortletURLFactoryUtil;
+import com.liferay.portal.kernel.portlet.configuration.permission.propagation.PortletConfigurationPermissionPropagation;
+import com.liferay.portal.kernel.portlet.configuration.permission.propagation.PortletConfigurationPermissionPropagationTracker;
 import com.liferay.portal.kernel.portlet.url.builder.PortletURLBuilder;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
@@ -304,6 +306,13 @@ public class PortletConfigurationPermissionsDisplayContext {
 			_httpServletRequest, "modelResourceDescription");
 
 		return _modelResourceDescription;
+	}
+
+	public PortletConfigurationPermissionPropagation
+		getPortletConfigurationPermissionPropagation() {
+
+		return PortletConfigurationPermissionPropagationTracker.
+			getPortletConfigurationPermissionPropagation(_getPortletResource());
 	}
 
 	public String getResourcePrimKey() throws ResourcePrimKeyException {
