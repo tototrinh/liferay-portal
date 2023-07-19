fb908a725884238495313f8032dc84210549075a
Quan Huynh <quan.huynh@codeenginestudio.com>
2023-07-19 16:53:50 +0700

    LPS-154515 Update the permission propagation value when updating in the permission modal

--- portal-kernel/src/com/liferay/portal/kernel/portlet/configuration/permission/propagation/BasePortletConfigurationPermissionPropagation.java
+++ portal-kernel/src/com/liferay/portal/kernel/portlet/configuration/permission/propagation/BasePortletConfigurationPermissionPropagation.java
@@ -68,6 +68,10 @@ public abstract class BasePortletConfigurationPermissionPropagation
 	public void updatePermissionPropagation(
 		ActionRequest actionRequest, ActionResponse actionResponse) {
 
+		if (!isAvailable(actionRequest)) {
+			return;
+		}
+
 		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
 			WebKeys.THEME_DISPLAY);
 
