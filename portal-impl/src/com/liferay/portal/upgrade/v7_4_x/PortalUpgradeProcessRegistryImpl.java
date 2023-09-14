cc298d393d47b0b8998c773c59ea7426629d136f
locpham97 <loc.pham@codeenginestudio.com>
2023-09-14 11:39:24 +0700

    LPS-154515 Upgrade Process for PropagationPermissionEntry table

--- portal-impl/src/com/liferay/portal/upgrade/v7_4_x/PortalUpgradeProcessRegistryImpl.java
+++ portal-impl/src/com/liferay/portal/upgrade/v7_4_x/PortalUpgradeProcessRegistryImpl.java
@@ -322,6 +322,9 @@ public class PortalUpgradeProcessRegistryImpl
 			new UpgradePartitionedControlTable("ClassName_"),
 			UpgradeModulesFactory.create(
 				new String[] {"com.liferay.comment.web"}, null));
+
+		upgradeVersionTreeMap.put(
+			new Version(28, 0, 0), new UpgradePropagationPermissionEntry());
 	}
 
 }
